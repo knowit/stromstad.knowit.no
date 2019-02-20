727ed31b179a63f9d1c1357755fe31cf27cf0abd
Christian Peter <christian150790@gmail.com>
2019-02-20 14:25:49 +0100

    Adds pics

--- ghpagesScript.js
+++
@@ -1,6 +0,0 @@
-const ghpages = require('gh-pages');
-
-ghpages.publish('public', {
-  repo: 'https://' + process.env.GITHUB_TOKEN + '@github.com:knowit/stromstad.knowit.no.git',
-  silent: true
-}, () => {});
